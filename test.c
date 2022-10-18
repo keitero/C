0a93e44bf013dd961b0ed5f83f46de3f4f72adf0
keitero <116052232+keitero@users.noreply.github.com>
2022-10-18 21:39:46 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,11 @@
+//写主函数（main）,作为代码的入口，函数后面都带有（）。一个工程中有且仅有一个main函数
+#include <stdio.h>//头文件
+
+int main()//int（整形integer）函数返回类型  main函数名
+{
+	printf("hello world"); //printf库函数，打印。这是引用别人的函数，需要引用头文件
+	                      
+	return 0;
+}
+//运行（ctrl+f5)? 
+//程序执行太快没看到——调整属性的子系统？？
